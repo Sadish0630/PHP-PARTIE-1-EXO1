5d9720878af1b51c36f3e366b1ad8233f3734331
audemar <audemarsadish@gmail.com>
2021-12-07 11:20:29 +0100

    exo1 terminé

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 1</title>
+</head>
+<body>
+    <p>
+   <?php
+   $name = 'Sadish';
+   echo $name;
+   ?>
+   </p>
+</body>
+</html>
